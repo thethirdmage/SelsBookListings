c358edf7f9b2114dace2955907eaa504404eca62
Selene Marivaldi <bobdablob42@gmail.com>
2018-06-25 11:54:46 -0400

    Add basic implementation of BookViewModel

--- app/src/main/java/com/selsapps/selsbooklistings/BookViewModel.java
+++ app/src/main/java/com/selsapps/selsbooklistings/BookViewModel.java
@@ -1,7 +1,23 @@
 package com.selsapps.selsbooklistings;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+
 public class BookViewModel extends ViewModel {
+    private MutableLiveData<ArrayList<Book>> books;
+    public LiveData<ArrayList<Book>> getBooks() {
+        if (books == null) {
+            books = new MutableLiveData<ArrayList<Book>>();
+            loadBooks();
+        }
+        return books;
+    }
+
+    private void loadBooks() {
+        // TODO: implement this method
+    }
 
 }
